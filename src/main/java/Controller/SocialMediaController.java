e96f2c77a6ce692a6de908f965b9507b42b229e1
Yohannes Itanna <itannayohannes@gmail.com>
2025-04-15 16:35:06 +0000

    Inital Commit Social Media Blog API Project

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -1,7 +1,12 @@
 package Controller;
 
+import Model.Account;
+import Model.Message;
+import Service.AccountService;
+import Service.MessageService;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+import java.util.List;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller. The endpoints you will need can be
@@ -14,9 +19,27 @@ public class SocialMediaController {
      * suite must receive a Javalin object from this method.
      * @return a Javalin app object which defines the behavior of the Javalin controller.
      */
+    AccountService accountService;
+    MessageService messageService;
+
+    public SocialMediaController() {
+        this.accountService = new AccountService();
+        this.messageService = new MessageService();
+    }
+
     public Javalin startAPI() {
         Javalin app = Javalin.create();
-        app.get("example-endpoint", this::exampleHandler);
+
+        //app.get("example-endpoint", this::exampleHandler);
+
+        app.post("/register", this::registerHandler);
+        app.post("/login", this::loginHandler);
+        app.post("/messages", this::createMessageHandler);
+        app.get("/messages", this::getAllMessagesHandler);
+        app.get("/messages/:message_id", this::getMessageByIdHandler);
+        app.delete("/messages/:message_id", this::deleteMessageHandler);
+        app.patch("/messages/:message_id", this::updateMessageHandler);
+        app.get("/accounts/:account_id/messages", this::getMessagesByAccountHandler);
 
         return app;
     }
@@ -25,9 +48,75 @@ public class SocialMediaController {
      * This is an example handler for an example endpoint.
      * @param context The Javalin Context object manages information about both the HTTP request and response.
      */
-    private void exampleHandler(Context context) {
-        context.json("sample text");
+    //private void exampleHandler(Context context) {
+        //context.json("sample text");
+    //}
+
+    private void registerHandler(Context context) {
+        Account account = context.bodyAsClass(Account.class);
+        Account result = accountService.registerAccount(account);
+        if (result!=null) {
+            context.json(result);
+        } else {
+            context.status(400);
+        }
     }
 
+    private void loginHandler(Context context) {
+        Account account = context.bodyAsClass(Account.class);
+        Account result = accountService.loginAccount(account);
+        if (result!=null) {
+            context.json(result);
+        } else {
+            context.status(401);
+        }
+    }
 
+    private void createMessageHandler(Context context) {
+        Message message = context.bodyAsClass(Message.class);
+        Message result = messageService.createMessage(message);
+        if (result != null) {
+            context.json(result);
+        } else {
+            context.status(400);
+        }
+    }
+
+    private void getAllMessagesHandler(Context context) {
+        List<Message> messages = messageService.getAllMessages();
+        context.json(messages);
+    }
+
+    private void getMessageByIdHandler(Context context) {
+        int messageId = Integer.parseInt(context.pathParam("message_id"));
+        Message message = messageService.getMessageById(messageId);
+        if (message != null) {
+            context.json(message);
+        }
+    }
+
+    private void deleteMessageHandler(Context context) {
+        int messageId = Integer.parseInt(context.pathParam("message_id"));
+        Message deleted = messageService.deleteMessage(messageId);
+        if (deleted != null) {
+            context.json(deleted);
+        }
+    }
+
+    private void updateMessageHandler(Context context) {
+        int messageId = Integer.parseInt(context.pathParam("message_id"));
+        Message input = context.bodyAsClass(Message.class);
+        Message updated = messageService.updateMessage(messageId, input.getMessage_text());
+        if (updated != null) {
+            context.json(updated);
+        } else {
+            context.status(400);
+        }
+    }
+
+    private void getMessagesByAccountHandler(Context context) {
+        int accountId = Integer.parseInt(context.pathParam("account_id"));
+        List<Message> messages = messageService.getMessagesByAccountId(accountId);
+        context.json(messages);
+    }
 }
---
+++ src/main/java/DAO/AccountDAO.java
@@ -0,0 +1,51 @@
+package DAO;
+
+import Model.Account;
+import Util.ConnectionUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AccountDAO {
+    public Account createAccount(Account account) {
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            String sql = "INSERT INTO account (username, password) VALUES (?, ?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, account.getUsername());
+            preparedStatement.setString(2, account.getPassword());
+            preparedStatement.executeUpdate();
+
+            ResultSet rs = preparedStatement.getGeneratedKeys();
+            if (rs.next()) {
+                return new Account(rs.getInt(1), account.getUsername(), account.getPassword());
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public Account getAccountById(int accountId) {
+        Connection connection = ConnectionUtil.getConnection();
+        try {
+            String sql = "SELECT * FROM account WHERE account_id = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, accountId);
+            ResultSet rs = preparedStatement.executeQuery();
+
+            if (rs.next()) {
+                return new Account(
+                    rs.getInt("account_id"),
+                    rs.getString("username"),
+                    rs.getString("password")
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
